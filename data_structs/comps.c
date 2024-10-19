1d4d5c8994e944effa25f719a1ffee406ac03d30
Yassir El yaqine <yel-yaqi@e3r9p11.1337.ma>
2024-10-19 11:32:57 +0100

    @ plane intersection @

--- data_structs/comps.c
+++ data_structs/comps.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 00:38:01 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/10/19 11:01:24 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/10/19 11:32:23 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,7 @@ t_comps 	prepare_computations(t_xs_list *intersection, t_ray ray)
 	if (intersection->object.form == SPHERE)
 		comps.normalv = normal_at(*(t_sphere *)comps.object.object, comps.point);
 	else if (intersection->object.form == PLANE)
-	{
-		printf("plane\n");
 		comps.normalv = vector(0, 1, 0);
-		
-	}
-	
 	if (vec_dot(comps.normalv, comps.eyev) < 0)
 	{
 		comps.inside = true;
